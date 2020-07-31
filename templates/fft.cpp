029a043c7f68e12bed409c43516baf4b4da8c5fb
prabowo02 <prabowo1048576@gmail.com>
2020-08-01 01:09:31 +0800

    Improve fft template

--- templates/fft.cpp
+++ templates/fft.cpp
@@ -1,86 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int mod = 1000000007;
-const int maxn = 100005;
+const int MOD = 1000000007;
+const int MAXN = 1 << 16;
 
 typedef complex<double> ftype;
-const double pi = acos(-1);
-ftype w[maxn];
+const double PI = acos(-1);
+ftype w[MAXN];
 
-template<typename T>
-void fft(T *in, T *out, int n, int k)
-{
-    if(n == 1)
-    {
-        *out = *in;
-        return;
+void init() {
+  for (int i = 0; i < MAXN; ++i) {
+    w[i] = ftype(cos(2*PI*i / MAXN), sin(2*PI*i / MAXN));
+  }
+}
+
+void fft(int n, vector<ftype> &a) {
+  for (int i = 1, j = 0; i < n; ++i) {
+    int bit = n >> 1;
+    for (; j >= bit; bit >>= 1) j -= bit;
+    j += bit;
+    if (i < j) swap(a[i], a[j]);
+  }
+
+  for (int len=2; len<=n; len<<=1) {
+    int wlen = MAXN/len;
+    for (int i=0; i<n; i+=len) {
+      for (int j=0; j<len>>1; j++) {
+        ftype u = a[i+j];
+        ftype v = a[i+j + len/2] * w[wlen * j];
+
+        a[i+j] = u + v;
+        a[i+j + len/2] = u - v;
+      }
     }
-    int t = maxn / n;
-    n /= 2;
-    fft(in, out, n, 2 * k);
-    fft(in + k, out + n, n, 2 * k);
-    for(int i = 0, j = 0; i < n; i++, j += t)
-    {
-        ftype t = w[j] * out[i + n];
-        out[i + n] = out[i] - t;
-        out[i] = out[i] + t;
-    }
+  }
 }
 
 const int sz = 16, msk = (1 << 16) - 1;
 
-vector<int> mul(vector<int> a, vector<int> b)
-{
-    int n = a.size() + b.size();
-    while(__builtin_popcount(n) != 1)
-        n++;
-    while(a.size() < n)
-        a.push_back(0);
-    while(b.size() < n)
-        b.push_back(0);
-    vector<ftype> A(n), B(n);
-    for(int i = 0; i < n; i++)
-    {
-        A[i] = {a[i] & msk, a[i] >> sz};
-        B[i] = {b[i] & msk, b[i] >> sz};
-    }
-    vector<ftype> nA(n), nB(n);
-    ftype w = polar(1., 2 * pi / n);
-    fft(A.data(), nA.data(), n, 1);
-    fft(B.data(), nB.data(), n, 1);
-    
-    ftype im2 = {0, 2}, im = {0, 1};
-    
-    for(int i = 0; i < n; i++)
-    {
-        ftype lA = (nA[i] + conj(nA[(n - i) % n])) / 2.;
-        ftype gA = (nA[i] - conj(nA[(n - i) % n])) / im2;
-        ftype lB = (nB[i] + conj(nB[(n - i) % n])) / 2.;
-        ftype gB = (nB[i] - conj(nB[(n - i) % n])) / im2;
-        A[i] = lA * lB + im * gA * gB;
-        B[i] = lA * gB + gA * lB;
-    }
-    fft(A.data(), nA.data(), n, 1);
-    fft(B.data(), nB.data(), n, 1);
-    reverse(begin(nA) + 1, end(nA));
-    reverse(begin(nB) + 1, end(nB));
-    vector<int> ans(n);
-    for(int i = 0; i < n; i++)
-    {
-        int a = llround(nA[i].real() / n) % mod;
-        int b = llround(nB[i].real() / n) % mod;
-        int c = llround(nA[i].imag() / n) % mod;
-        ans[i] = (a + (b << sz) + (c << 2 * sz)) % mod;
-    }
-    while(ans.size() > 1 && ans.back() == 0)
-        ans.pop_back();
-    while(ans.size() > 100001)
-        ans.pop_back();
-    return ans;
-}
+vector<int> multiply(vector<int> a, vector<int> b) {
+  int n = 1 << (32 - __builtin_clz(a.size() + b.size()));
+  a.resize(n, 0); b.resize(n, 0);
 
-int main() {
+  vector<ftype> A(n), B(n);
+  for(int i = 0; i < n; i++) {
+    A[i] = ftype(a[i] & msk, a[i] >> sz);
+    B[i] = ftype(b[i] & msk, b[i] >> sz);
+  }
+
+  fft(n, A); fft(n, B);
   
-  return 0;
+  ftype im2 = {0, 2}, im = {0, 1};
+  
+  vector<ftype> nA(n), nB(n);
+  for(int i = 0; i < n; i++) {
+    ftype lA = (A[i] + conj(A[(n - i) % n])) / 2.;
+    ftype gA = (A[i] - conj(A[(n - i) % n])) / im2;
+    ftype lB = (B[i] + conj(B[(n - i) % n])) / 2.;
+    ftype gB = (B[i] - conj(B[(n - i) % n])) / im2;
+    nA[i] = lA * lB + im * gA * gB;
+    nB[i] = lA * gB + gA * lB;
+  }
+  fft(n, nA); fft(n, nB);
+  reverse(begin(nA) + 1, end(nA));
+  reverse(begin(nB) + 1, end(nB));
+  vector<int> ans(n);
+  for(int i = 0; i < n; i++) {
+    long long a = llround(nA[i].real() / n) % MOD;
+    long long b = llround(nB[i].real() / n) % MOD;
+    long long c = llround(nA[i].imag() / n) % MOD;
+    ans[i] = (a + (b << sz) + (c << 2 * sz)) % MOD;
+  }
+
+  ans.resize(a.size() + b.size() - 1);
+  return ans;
 }
